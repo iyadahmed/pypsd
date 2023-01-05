63815178d7544c8f88bbb1be33c1dbaf49d2a850
Iyad Ahmed <iyadahmed430@gmail.com>
2023-01-05 08:17:04 +0200

    Get PSD file path from program arguments

--- demo.py
+++ demo.py
@@ -1,4 +1,15 @@
+import sys
+
 from pypsd.reader import read_psd
 
+
+def main():
+    if len(sys.argv) != 2:
+        print("Expected arguments: file.psd")
+        return
+
+    read_psd(sys.argv[1])
+
+
 if __name__ == "__main__":
-    read_psd("/home/iyad/Desktop/placeholders-with-frames_ungrouped_color.psd")
+    main()
